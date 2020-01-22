5ad8c69adbf4ed402e43e9fd6d476728c1e75a34
alsdede <andreluizsilveira.88@gmail.com>
2020-01-22 15:38:59 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>teste2</Text>
+        </View>
+    );
+}
